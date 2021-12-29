adf8afaac5d7e3dea925b70e68fb465ced60633f
Pranish33 <Pranishlink33@gmail.com>
2021-12-29 21:39:43 +0545

    updated comments
    Only comments are updated

--- binary_search_algorithm.py
+++ binary_search_algorithm.py
@@ -1,10 +1,12 @@
+#defining function for the binary search 
 def binary_search_algorithm(list, target):
-
+    #Creating variable for last and first element in array
     first = 0
     last = len(list) - 1
-
+    #comparing first and last element 
+    
     while first <= last:
-
+        # Initialing mid-point element and search for the searched element  
         midpoint = (first+last) // 2
 
         if list[midpoint] == target:
@@ -18,6 +20,7 @@ def binary_search_algorithm(list, target):
 
     return None
 
+#another method for verifying binary_search_algorithm
 def verify(index):
     if index is not None:
         print("Target found at index", index)
